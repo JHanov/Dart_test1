52936afd846e600c2e8aac246859d4cb62aab2b9
JHanov <hanovjeremy@gmail.com>
2022-03-16 12:13:47 +0700

    nomor 1
    nomor1

--- Dart_test1/dart_test_1/bin/dart_test_1.dart
+++ Dart_test1/dart_test_1/bin/dart_test_1.dart
@@ -1,3 +1,19 @@
+import 'dart:io';
+
+//Link Github= https://github.com/JHanov/Dart_test1
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('Masukan angka yang ingin dicari faktornya: ');
+  String? angka = stdin.readLineSync();
+  int a = int.parse(angka!);
+  if (a < 1) {
+    print('Angka dibawah 1, masukan angka diatas 1');
+    return;
+  } else {
+    print('Faktornya adalah\n');
+    for (int i = 1; i <= a; i++) {
+      if (a % i == 0) {
+        print('$i\n');
+      }
+    }
+  }
 }
